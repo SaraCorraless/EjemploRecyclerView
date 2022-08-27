66b3daef939079c20aaa168e8d0571052a0fec22
sara <saracorraless01@gmail.com>
2022-08-27 18:25:41 +0200

    add cardView

--- app/src/main/java/com/cys/ejemplorecyclerview/MyAdapter.kt
+++ app/src/main/java/com/cys/ejemplorecyclerview/MyAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class MyAdapter(val item:ArrayList<String>, val context:Context) :RecyclerView.Adapter<MyAdapter.ViewHolder>(){
@@ -18,6 +19,11 @@ class MyAdapter(val item:ArrayList<String>, val context:Context) :RecyclerView.A
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.items.text = item.get(position)
+
+        //listener en el titulo
+        holder.items.setOnClickListener{
+            Toast.makeText(context, holder.items.text, Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun getItemCount(): Int {
